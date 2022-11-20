c81067d6e1b73815e9388d4ccb784a00e4c4ea06
Inessa <inessa.boickova@yandex.ru>
2022-11-20 22:38:18 +0300

    create 404 page

--- src/components/404.js
+++ src/components/404.js
@@ -4,7 +4,7 @@ const Page404 = () => {
     return (
         <div>
             <p style={{'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px','color': 'white'}}>Page doesn't exist</p>
-            <Link style={{'display': 'block', 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px', 'marginTop': '30px','color': 'white'}} to="/ToDo-React-API/todo">Back to main page</Link>
+            <Link style={{'display': 'block', 'textAlign': 'center', 'fontWeight': 'bold', 'fontSize': '24px', 'marginTop': '30px','color': 'white'}} to="/ToDo-React-API">Back to main page</Link>
         </div>
     )
 }
